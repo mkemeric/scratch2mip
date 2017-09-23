d2b03e93edb72cbe7c796bc806f3759d9d864169
mkemeric <mkemeric@gmail.com>
2017-09-22 23:43:52 -0500

    Looking for random 10 in play sound

--- scratch2mip.js
+++ scratch2mip.js
@@ -56,10 +56,10 @@
     var descriptor = {
         blocks: [
             [' ', 'MiP: ' + locale[lang].turn_right, 'right', 90],
-            [' ', 'MiP: ' + locale[lang].turn_left, 'left', 88],
+            [' ', 'MiP: ' + locale[lang].turn_left, 'left', 90],
             [' ', 'MiP: ' + locale[lang].move_forward, 'forward'],
             [' ', 'MiP: ' + locale[lang].move_backward, 'backward'],
-            [' ', 'MiP: ' + locale[lang].play_sound, 'talk']
+            [' ', 'MiP: ' + locale[lang].play_sound, 'talk', 10]
         ]
     };
 
